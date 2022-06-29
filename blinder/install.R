3e09a87d1d8f96832a72d0e0bafa986b38c18f0c
Jianliang <j.gao@imperial.ac.uk>
2022-06-30 00:43:35 +0100

    Delete blinder directory

--- blinder/install.R
+++
@@ -1,7 +0,0 @@
-install.packages("highr")
-install.packages("markdown")
-install.packages("stringr")
-install.packages("yaml")
-install.packages("knitr")
-install.packages("rmarkdown")
-install.packages("rstan")
