925a5b0d804450335649f8055d201620bbef4bd7
Jianliang <j.gao@imperial.ac.uk>
2022-06-30 00:42:57 +0100

    Create install.R

---
+++ binder/install.R
@@ -0,0 +1,8 @@
+install.packages("dplyr")
+install.packages("highr")
+install.packages("markdown")
+install.packages("stringr")
+install.packages("yaml")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("rstan")
