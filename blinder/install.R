ea30e9cbafed55e7ab6f1ead1936dc1da5ff3fe8
Jianliang <j.gao@imperial.ac.uk>
2022-06-29 23:40:42 +0100

    Create install.R

---
+++ blinder/install.R
@@ -0,0 +1,7 @@
+install.packages("highr")
+install.packages("markdown")
+install.packages("stringr")
+install.packages("yaml")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("rstan")
